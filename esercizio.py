7ee96e0cace4e1ed35428a14dcd76b1ae3c692b1
Sara3-gif <saragritti03@gmail.com>
2024-10-30 15:38:43 +0100

    Update esercizio.py

--- esercizio.py
+++ esercizio.py
@@ -1,11 +1,23 @@
 import pandas as pd 
 import numpy as np 
-#lettura del file dati 
-df = pd.read_csv('Nemo_6670.dat', sep=' ')
-#creiamo una classe 
+import matplotlib.pyplot as plt
 class Stelle:
-  def _init_(self, M_ass, b_y, age_parent):
-    self.M_ass = M_ass
-    self.b_y = b_y
-    self.age_parent = age_parent 
+  def __init__(self, Nemo_6670.dat):
+   self.data = pd.read_csv(Nemo_6670.dat)
+   self.age_bins = [(0, 1), (1, 5), (5, np.inf)]  # Definire i range per i sottogruppi di età
+   self.age_labels = ["< 1 Gyr", "1 - 5 Gyr", "> 5 Gyr"]
     
+  def plot_color_magnitude(self):
+        plt.figure(figsize=(10, 6))
+        scatter = plt.scatter(
+            self.data['b-y'], 
+            self.data['M_ass'], 
+            c=self.data['age_parent'], 
+            cmap='viridis', 
+            alpha=0.6
+        )
+        plt.colorbar(scatter, label='Età (Gyr)')
+        plt.xlabel("Colore (b-y)")
+        plt.ylabel("Magnitudine (M_ass)")
+        plt.title("Diagramma Colore-Magnitudine delle Stelle")
+        plt.show()
