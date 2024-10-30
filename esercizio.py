10138600654034ba1b1b364d2dbb33d121284cc0
Sara3-gif <saragritti03@gmail.com>
2024-10-30 15:20:00 +0100

    Update esercizio.py

--- esercizio.py
+++ esercizio.py
@@ -1,5 +1,7 @@
-#lettura file 
 import pandas as pd 
 import numpy as np 
-
-df=pd.read.cvs
+#lettura del file dati 
+df = pd.read_csv('Nemo_6670.dat', sep=' ')
+#creiamo una classe 
+class Stelle:
+  def _init_():
